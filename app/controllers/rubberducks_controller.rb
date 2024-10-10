ecb15588eb332f544ec94807206cb3d7a88f44d2
joseph heron <jheronn77@gmail.com>
2024-10-10 14:21:17 +0100

    index, new and create routes added

--- app/controllers/rubberducks_controller.rb
+++ app/controllers/rubberducks_controller.rb
@@ -1,2 +1,27 @@
 class RubberducksController < ApplicationController
+
+  def index
+    @rubberducks = Rubberduck.all
+  end
+
+  def new
+    @rubberduck = Rubberduck.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @rubberduck = Rubberduck.new(rubberduck_params)
+    @rubberduck.user = @user
+    if @rubberduck.save
+      redirect_to rubberducks_path
+    else
+      render "rubberducks/index", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def rubberduck_params
+    params.require(:rubberduck).permit(:name, :city, :outfit, :price)
+  end
 end
