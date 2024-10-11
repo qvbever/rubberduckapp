b24d64e9bde432ceda8ad9922f018d6e3c0d7d72
Lisa Doerner <liza.doerner@gmail.com>
2024-10-11 18:08:04 +0200

    Created a rubberduck component card for index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Rubberduck.create([
+  { name: 'Jack', city: 'Tortuga', outfit: "pirate", price: 10.00 },
+  { name: 'Pippi', outfit: 'Longstocking', city: 'Smaland', price: 15.00 },
+])
---
+++ test/components/rubber_duck_card_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class RubberDuckCardComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(RubberDuckCardComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
